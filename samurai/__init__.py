284cedc7a801a4c229fdc6f1c6f9e06bf0299f48
c4ffein <c4ffein>
2021-12-01 00:17:43 +0100

    v0.1.1: pretty_tb middleware

--- samurai/__init__.py
+++ samurai/__init__.py
@@ -1,13 +1,15 @@
 """?"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
-from samurai.settings import get_env_debug_secret_hosts, get_env_databases, get_env_email
+from .settings import get_env_debug_secret_hosts, get_env_databases, get_env_email
+from . import middleware
 
 
 __all__ = [
     "get_env_debug_secret_hosts",
     "get_env_databases",
     "get_env_email",
+    "middleware",
 ]
